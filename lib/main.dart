f62fedc452ea1f9999c330222ceac0ca2b5f18ca
bangnguyen2264 <2251120335@ut.edu.vn>
2025-03-13 15:25:04 +0700

    feat: add app icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:study_tracker_mobile/app/app.dart';
 import 'package:study_tracker_mobile/app/di.dart';
-import 'package:study_tracker_mobile/presentation/resources/bloc_provider_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
